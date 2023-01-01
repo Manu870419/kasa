1a777ebaf3ef25dcfdd05ca56a8f994723b84531
BEGHIN <beghinmanu870419@gmail.com>
2023-01-01 16:43:31 +0100

    Footer.jsx et importation de Footer dans app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // react framework
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+
 import "./main.css";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
+import SingleProduct from './pages/Singleproduct';
 import PageNotFound from "./pages/PageNotFound";
 
 import StyledNavbar from './components/StyledNavbar';
+import Footer from './components/Footer';
 
 function App() {
     return(
@@ -17,8 +21,11 @@ function App() {
                     <Routes>
                         <Route index element={<Home />} />
                         <Route path="about" element={ <About />} />
+                        <Route path="products/:productId" element={<SingleProduct />} />
+                        <Route path="/kasa" element={<Navigate to="/" replace />} />
                         <Route path="*" element={<PageNotFound />} />
                     </Routes>
+                    <Footer />
                 </Router>
             </div>
         </div>
