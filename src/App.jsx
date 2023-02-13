8c783ce95ed13c3c0a190d065145b2ec75fc252f
BEGHIN <beghinmanu870419@gmail.com>
2023-02-13 18:03:00 +0100

    Rectification App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Routes, Navigate, } from 'react-router-dom';
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import SingleProduct from './pages/Singleproducts';
+import SingleProduct from "./pages/Singleproduct";
 import Error from "./pages/Error";
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
                 {/* path="*" permet de renvoyé vers la page 404 ou vers une page prédéfinit
       si l'utilisateur se "perd" */}
                 <Route path="/About" element={<About />} />
-                <Route path="/Singleproducts/:id" element={<SingleProduct />} />
+                <Route path="/products/:id" element={<SingleProduct />} />
                 <Route path="/kasa" element={<Navigate to="/" replace />} />
                 <Route path="*" element={<Error />} />
             </Routes>
