d4fbe57badb396d32b8b05c6c3d702fc8be4958e
BEGHIN <beghinmanu870419@gmail.com>
2023-02-17 22:31:05 +0100

    Rectification _error.scss

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import image from "../assets/logo-error.png";
 
 function Error() {
     return (
         <main className="error">
-            <div className="error__content">
-                <p className="error__number">404</p>
-                <p className="error__text">
-                    " Oups ! La page que vous demandez n'existe pas."
-
-                </p>
-            </div>
+            <img src={image} alt="404" className="error-logo" />
+            <p className="error__text">
+                Oups ! La page que vous demandez n'existe pas.
+            </p>
             <Link to="/" className="error__link">
                 Retourner sur la page d'acceuil
             </Link>
