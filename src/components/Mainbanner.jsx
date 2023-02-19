d51f33248a32425ccda0ea2113ad76b8f71070d8
BEGHIN <beghinmanu870419@gmail.com>
2023-02-19 16:12:51 +0100

    Rectification photos page accueil + carrousel

--- src/components/Mainbanner.jsx
+++ src/components/Mainbanner.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 function Mainbanner({ image, title }) {
     return (
+
         <section className="homeBanner">
             <img src={image} alt="Paysage" className="thunb__img" />
             <div className="homeBanner__overlay"></div>
             <span className="homeBanner__title">{title}</span>
         </section >
+
     );
 };
 
